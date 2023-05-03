7c5f1f978a1a38acdf103684907017ae3ea875aa
Vladyslav Korzh <tastetesting190599kvg@gmail.com>
2023-05-03 19:21:49 +0300

    comma added

--- BeautySalon/settings.py
+++ BeautySalon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'salon.apps.SalonConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
     'adminpanel.apps.AdminpanelConfig'
 ]
 
