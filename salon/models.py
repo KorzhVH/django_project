c1161c4b5762ed9f5b0f06f2530aeec143ef8f70
Vladyslav Korzh <tastetesting190599kvg@gmail.com>
2023-05-15 05:06:23 +0300

    services

--- salon/models.py
+++ salon/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-    time_required = models.CharField(max_length=100)
+    required_time = models.CharField(max_length=100)
     price = models.IntegerField()
 
     def __str__(self):
